ce3157c9bba1cc2a428a93c2afb24fdfe4528f92
Malatawy15 <malatawy15@gmail.com>
2014-10-15 19:43:40 +0200

    Add Solution.java

---
+++ src/search/Solution.java
@@ -0,0 +1,15 @@
+package search;
+
+public class Solution {
+	
+	SearchTreeNode goalNode;
+	int cost;
+	int numNodes;
+	
+	public Solution(SearchTreeNode goalNode, int cost, int numNodes) {
+		this.goalNode = goalNode;
+		this.cost = cost;
+		this.numNodes = numNodes;
+	}
+
+}
