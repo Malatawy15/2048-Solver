0e3fbcaeca13b6e9e38ec7ca5bfcf30915e0b4d2
AmrARaouf <amr.abdelraouf93@gmail.com>
2014-10-15 17:22:54 +0200

    adds grid functionality

--- src/problem/Operators.java
+++ src/problem/Operators.java
@@ -1,5 +1,5 @@
 package problem;
 
 public interface Operators {
-
+	
 }
--- src/problem/Operators2048.java
+++ src/problem/Operators2048.java
@@ -1,9 +1,7 @@
 package problem;
 
 public class Operators2048 implements Operators {
-	
-	public Operators operators;
-	
+		
 	public enum Operators {
 		UP,
 		DOWN,
