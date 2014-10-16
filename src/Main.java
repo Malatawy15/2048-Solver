552c7368eb9e8d1403aea4fc2c39cbd37e24a792
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2014-10-16 23:46:53 +0200

    fixing heuristic

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import search.Solution;
 public class Main {
 
 	public static void main(String[] args) {
-		Problem2048 problem = new Problem2048(4);
+		Problem2048 problem = new Problem2048(11);
 		Solution s = problem.solve("GR1", true);
 		System.out.println(s.getGoalNode().toString());
 		System.out.println(s.getGoalNode().returnPath());
